bb5c64e864689bd4eb5652123b7a7826ebf783ba
chiziivictor <chizivwokoma@gmail.com>
2022-07-21 23:43:40 +0100

    esbuild configuration

---
+++ src/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+import * as esbuild from "esbuild-wasm";
+export const unpkgPathPlugin = () => {
+  return {
+    name: "unpkg-path-plugin",
+    setup(build: esbuild.PluginBuild) {
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        console.log("onResole", args);
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        }
+
+        if (args.path.includes("./") || args.path.includes("../")) {
+          return {
+            path: new URL(
+              args.path,
+              "https://unpkg.com" + args.resolveDir + "/"
+            ).href,
+            namespace: "a",
+          };
+        }
+
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log("onLoad", args);
+        if (args.path === "index.js") {
+          return {
+            loader: "jsx",
+            contents: `
+              import react, {useState} from "react";
+              console.log(react, useState);
+            `,
+          };
+        }
+
+        const { data, request } = await axios.get(args.path);
+        return {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+      });
+    },
+  };
+};
