4324d4db0b77fa12761ceb8e4bd7ff6ed805ce3a
Design Synchrony <103902479+designsynchrony@users.noreply.github.com>
2022-07-26 03:31:48 +0100

    code refactoring and folder restructuring

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import bundle from '../bundler';
+import CodeEditor from './code-editor';
+import Preview from './preview';
+
+const CodeCell = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const onClick = async () => {
+    const output = await bundle(input);
+    setCode(output);
+  };
+
+  return (
+    <div>
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+      />
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <Preview code={code} />
+    </div>
+  );
+};
+
+export default CodeCell;
